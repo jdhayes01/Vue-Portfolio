36a264a99292ee5b56726f75842fa3fe33b53f62
James Hayes <owner@jamesdewitthayes.com>
2020-07-23 14:44:43 -0700

    added packages

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,8 @@
 <template>
-    <H1>Header</H1>
+    <div>
+        <b-nav>
+            <b-nav-item active>Fancy Quiz App</b-nav-item>
+            <b-nav-item>Counter</b-nav-item>
+        </b-nav>
+    </div>
 </template>
